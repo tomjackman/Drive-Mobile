7f2a81709b2580ca1bdd8bde814625fdb3953050
Tom Jackman <tom.dls@hotmail.com>
2016-01-29 16:22:16 +0000

    Fixed DOB not showing on menu

--- www/js/menuController.js
+++ www/js/menuController.js
@@ -2,7 +2,7 @@ angular.module('starter')
 
 .controller('menuController', function($scope, $cordovaBluetoothSerial, $ionicModal, BluetoothService, StorageService) {
 
-$scope.birthYear = localStorage.getItem('birthYear');
+$scope.birthYear = localStorage.getItem('dateOfBirth');
 $scope.gender = localStorage.getItem('gender');
 $scope.country = localStorage.getItem('country');
 
