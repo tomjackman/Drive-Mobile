374630eaa2789a39c8ead96d37de013a5c4ad241
Tom Jackman <tom.dls@hotmail.com>
2016-03-19 10:38:02 +0000

    Added REST call and QR scanner functionality.

--- www/js/followingController.js
+++ www/js/followingController.js
@@ -1,5 +1,33 @@
 angular.module('starter')
 
-.controller('followingController', function($scope, $stateParams) {
+.controller('followingController', function($scope, $stateParams, $cordovaBarcodeScanner, $http) {
+
+	$scope.vehicles = JSON.parse(localStorage.getItem('followingList'));
+
+	$scope.followVehicle = function(){
+
+      $cordovaBarcodeScanner.scan().then(
+          function (vehicleID) {
+             
+          	$http.get(localStorage.getItem('serverAddress') + '/Drive/vehicle/show/' + vehicleID.text + '.json').success(function(data) {
+
+	        
+	      	  var followingList = localStorage.getItem('followingList');
+              followingList = JSON.parse(followingList);
+              $scope.manufacturer = "";
+              var newVehicle = {carData: data};
+              followingList.push(newVehicle);
+              localStorage.setItem('followingList', JSON.stringify(followingList));
+
+              $scope.vehicles = JSON.parse(localStorage.getItem('followingList'));
+          });
+
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );
+
+    } 
 
 });
